7731b71d688f61d643240a8d602eb622718ed54f
한태영 <s46422574@gmail.com>
2026-04-27 13:48:45 +0900

    feat: 네이버 파비콘 호환을 위해 /favicon.ico 외부 URL 리다이렉트 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/favicon.ico",
+        destination:
+          "https://storage.googleapis.com/cr-resource/image/5a2e841ee3f3d6089198a6e917892c57/koreascofficial/koreascofficial-favicon.ico",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
